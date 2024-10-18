930206615a0cbf8b78805ed1aa9e04e9537c7236
theabinbenny <abin2358@gmail.com>
2024-10-18 15:54:12 +0530

    look into flacon and got-ocr

--- processing_pdf_and_txt/process_image_text.py
+++ processing_pdf_and_txt/process_image_text.py
@@ -1,13 +1,30 @@
-from PIL import Image
-import pytesseract
+from transformers import AutoModel, AutoTokenizer
+
+tokenizer = AutoTokenizer.from_pretrained('ucaslcl/GOT-OCR2_0', trust_remote_code=True)
+model = AutoModel.from_pretrained('ucaslcl/GOT-OCR2_0', trust_remote_code=True, low_cpu_mem_usage=True, device_map='cuda', use_safetensors=True, pad_token_id=tokenizer.eos_token_id)
+model = model.eval().cuda()
 
 
-# Load an image from file
-img = Image.open('processing_pdf_and_txt/files/sample_image_to_txt.jpg')
+# input your test image
+image_file = 'processing_pdf_and_txt/files/sample_image_to_txt.jpg'
 
-# Extract text from the image using pytesseract
-text = pytesseract.image_to_string(img)
+# plain texts OCR
+res = model.chat(tokenizer, image_file, ocr_type='ocr')
 
-# Print the extracted text
-print("Extracted Text:")
-print(text)
+# format texts OCR:
+# res = model.chat(tokenizer, image_file, ocr_type='format')
+
+# fine-grained OCR:
+# res = model.chat(tokenizer, image_file, ocr_type='ocr', ocr_box='')
+# res = model.chat(tokenizer, image_file, ocr_type='format', ocr_box='')
+# res = model.chat(tokenizer, image_file, ocr_type='ocr', ocr_color='')
+# res = model.chat(tokenizer, image_file, ocr_type='format', ocr_color='')
+
+# multi-crop OCR:
+# res = model.chat_crop(tokenizer, image_file, ocr_type='ocr')
+# res = model.chat_crop(tokenizer, image_file, ocr_type='format')
+
+# render the formatted OCR results:
+# res = model.chat(tokenizer, image_file, ocr_type='format', render=True, save_render_file = './demo.html')
+
+print(res)
